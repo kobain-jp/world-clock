a271644cabd92850d6cb75c4b516c795b6f38dcc
kobain-jp <55679081+kobain-jp@users.noreply.github.com>
2020-10-07 14:01:30 +0900

    add init files

---
+++ src/main/java/com/dojo/worldclock/WorldclockApplication.java
@@ -0,0 +1,13 @@
+package com.dojo.worldclock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldclockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldclockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dojo/worldclock/WorldclockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dojo.worldclock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldclockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
